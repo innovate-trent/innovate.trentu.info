fc942cbb82fd66af1549dba73cc6aa923af92712
Matthew Piercey <piercey.matthew@gmail.com>
2021-09-02 17:18:53 -0400

    add some basic content/sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
     purge: [ "./src/_includes/**/*.njk", "./src/*.html" ],
     darkMode: false, // or 'media' or 'class'
+    // mode: 'jit',
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'noto': ['Noto Sans', 'sans-serif'],
+          'heading': ['Staatliches', 'monospace'],
+        },
+
+        minHeight: (theme) => ({
+          ...theme('spacing')
+        }),
+      },
     },
     variants: {
       extend: {},
